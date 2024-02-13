0451eae94ca3c90df0d63372b0f9f3b3a4f6d50d
liabafi <62510359+liabafi@users.noreply.github.com>
2024-02-13 18:13:37 +0000

    Todo Completado.

--- main.py
+++ main.py
@@ -2,14 +2,16 @@
 import prob_calculator
 from unittest import main
 
-prob_calculator.random.seed(95)
-hat = prob_calculator.Hat(blue=4, red=2, green=6)
-probability = prob_calculator.experiment(
-    hat=hat,
-    expected_balls={"blue": 2,
-                    "red": 1},
-    num_balls_drawn=4,
-    num_experiments=3000)
+hat = prob_calculator.Hat(blue=4, red=2, green=6, black=10)
+
+probability = prob_calculator.experiment(hat=hat,
+                                         expected_balls={
+                                           "green": 1,
+                                           "blue": 1,
+                                           "black": 10
+                                         },
+                                         num_balls_drawn=15,
+                                         num_experiments=10000)
 print("Probability:", probability)
 
 # Run unit tests automatically
